08ad0cea73e03b27a11e4c00e6fcd8d076e874ef
Isaac Adams <isaacdadams@pm.me>
2022-12-31 09:48:26 -0500

    fix: reducing language

--- components/Landing.vue
+++ components/Landing.vue
@@ -66,10 +66,7 @@
             <div class="hidden lg:block">
               <img class="rounded opacity-75" src="/api-g1021c9066_1280.jpg" />
             </div>
-            <WelcomeItem
-              icon="fa-rocket"
-              title="I need a Custom Software Tool."
-            >
+            <WelcomeItem icon="fa-rocket" title="I need a Custom Software.">
               <div class="flex flex-col justify-center pr-3">
                 <p>
                   Whether you have a Software-as-a-Service (SaaS) idea, using
