f788a01c383a33f114ae1995ba210de5e0612b1e
Isaac Adams <isaacdadams@pm.me>
2022-12-30 19:10:55 -0500

    chore: adding footer

---
+++ components/GaltFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer items-center p-4 bg-neutral text-neutral-content">
+    <div class="items-center grid-flow-col">
+      <img width="36" height="36" src="/Galt-Code-01g-icon.png" />
+      <p>Copyright © 2022 - All right reserved</p>
+    </div>
+    <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+      <a href="https://www.linkedin.com/company/galt-code" target="_blank">
+        <i class="fa-brands fa-linkedin fa-2xl"></i>
+      </a>
+      <a href="https://github.com/galtcode" target="_blank">
+        <i class="fa-brands fa-github fa-2xl"></i>
+      </a>
+    </div>
+  </footer>
+</template>
