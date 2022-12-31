7c7ab9a1b04051f6a101b78492acceb7a8a789c8
Isaac Adams <isaacdadams@pm.me>
2022-12-31 10:57:25 -0500

    fix: better layout for footer in both desktop and mobile screens

--- components/GaltFooter.vue
+++ components/GaltFooter.vue
@@ -1,18 +1,24 @@
 <template>
-  <footer class="footer flex items-center p-4 bg-neutral text-neutral-content">
-    <div class="items-center grid-flow-col">
-      <img width="36" height="36" src="/icon.png" />
-    </div>
-    <div class="grid-flow-col gap-4 place-self-center justify-self-end">
-      <a href="https://www.linkedin.com/company/galt-code" target="_blank">
-        <i class="fa-brands fa-linkedin fa-2xl"></i>
-      </a>
-      <a href="https://github.com/galtcode" target="_blank">
-        <i class="fa-brands fa-github fa-2xl"></i>
-      </a>
-    </div>
-    <div class="flex flex-row">
-      <p>Copyright © 2022 - All rights reserved</p>
+  <footer
+    class="footer grid grid-row-2 items-center p-4 bg-neutral text-neutral-content justify-center"
+  >
+    <div class="grid grid-cols-2 max-w-[1200px] w-[100vw] px-3">
+      <div class="items-center grid-flow-col">
+        <img width="36" height="36" src="/icon.png" />
+      </div>
+      <div class="flex gap-4 place-self-center justify-self-end">
+        <a href="https://www.linkedin.com/company/galt-code" target="_blank">
+          <i class="fa-brands fa-linkedin fa-2xl"></i>
+        </a>
+        <a href="https://github.com/galtcode" target="_blank">
+          <i class="fa-brands fa-github fa-2xl"></i>
+        </a>
+      </div>
+      <div
+        class="col-span-2 w-full text-center py-2 flex justify-center align-center"
+      >
+        <p>Copyright © 2022 - All rights reserved</p>
+      </div>
     </div>
   </footer>
 </template>
