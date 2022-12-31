169a33692ff2d5e207d48402b91b198f7dafcd95
Isaac Adams <isaacdadams@pm.me>
2022-12-31 10:06:09 -0500

    fix: better footer layout for now

--- components/GaltFooter.vue
+++ components/GaltFooter.vue
@@ -1,16 +1,18 @@
 <template>
-  <footer class="footer items-center p-4 bg-neutral text-neutral-content">
+  <footer class="footer flex items-center p-4 bg-neutral text-neutral-content">
     <div class="items-center grid-flow-col">
       <img width="36" height="36" src="/icon.png" />
-      <p>Copyright © 2022 - All right reserved</p>
     </div>
-    <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+    <div class="grid-flow-col gap-4 place-self-center justify-self-end">
       <a href="https://www.linkedin.com/company/galt-code" target="_blank">
         <i class="fa-brands fa-linkedin fa-2xl"></i>
       </a>
       <a href="https://github.com/galtcode" target="_blank">
         <i class="fa-brands fa-github fa-2xl"></i>
       </a>
+    </div>
+    <div class="flex flex-row">
+      <p>Copyright © 2022 - All rights reserved</p>
     </div>
   </footer>
 </template>
