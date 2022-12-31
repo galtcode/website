935d3dfb6869793b9d51122a6871695774c69c2b
Isaac Adams <isaacdadams@pm.me>
2022-12-31 12:46:19 -0500

    feat: blog article page using slug

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <body class="w-screen flex justify-center">
+    <article class="prose">
+      <h1>{{ page.title }}</h1>
+      <nuxt-content :document="page" />
+    </article>
+  </body>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const page = await $content('articles/' + params.slug || 'hello').fetch()
+
+    return {
+      page,
+    }
+  },
+}
+</script>
